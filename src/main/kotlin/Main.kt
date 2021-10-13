4f40e2e74363c53431891db4f7cd5c664b15d0c8
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-10-13 20:31:56 +0300

    calc salary raise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,39 +5,49 @@ fun main(){
         will receive a 4% raise, and one rated poor will receive a 1.5% raise.
         */
     println("This program calc salary raise")
+    var salary:Double= 16000.0
+    var performance= 1
+    //if inputs from the user
+    /*
     println("Enter your salary")
-    var salary= readLine()?.toDouble()
+    var salary= readLine()!!.toDouble()
     println("Enter your performance rating (1=excellent, 2=good, and 3=poor)")
     var performance= readLine()
 
-    var newSalary:Double?=salary
+     */
 
-    if(performance.equals("1")){
-        newSalary=newSalary!!*1.6
+    var newSalary:Double=salary
+
+    if(performance==1){
+        newSalary *= 1.6
         println("Your new salary ${"%.4f".format(newSalary)}")
-    }else if(performance.equals("2")){
-        newSalary=newSalary!!*1.4
+    }else if(performance==2){
+        newSalary *= 1.4
         println("Your new salary ${"%.4f".format(newSalary)}")
-    }else if(performance.equals("3")){
-        newSalary=newSalary!!*1.15
+    }else if(performance==3){
+        newSalary*=1.15
         println("Your new salary ${"%.4f".format(newSalary)}")
-    }else{
+    }//if inputs from the user
+    /*
+    else{
         println("Please enter the right performance rating (1=excellent, 2=good, and 3=poor)")
     }
+
+     */
 
     //By using when statement
     /*
     when(performance){
-        "1"->{
-            newSalary=newSalary!!*1.6
+        1->{
+            newSalary*=1.6
             println("Your new salary ${"%.4f".format(newSalary)}")
         }
-        "2"->{
-            newSalary=newSalary!!*1.4
+        2->{
+            newSalary*=1.4
             println("Your new salary ${"%.4f".format(newSalary)}")
         }
-        "3"->{
-            newSalary=newSalary!!*1.15
+        3->{
+            newSalary*=1.15
             println("Your new salary ${"%.4f".format(newSalary)}")
         }
         else->{
