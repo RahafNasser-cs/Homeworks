3c2a0dfe4d353f44e6e69655ea71a422b6197eaf
Rahaf Nasser <rahaf.nasser.ruh.k@tuwaiq.edu.sa>
2021-10-13 23:10:07 +0300

    function/non-value-returning

---
+++ src/main/kotlin/Helpers.kt
@@ -0,0 +1,13 @@
+fun studentInfo(firstName:String, lastName:String, GPA:Double, major:String){
+    println(firstName+" "+lastName+" |\t"+GPA+" |\t"+major)
+}
+
+fun startProgram(){
+    println("Welcome to the students record!\n")
+    println("Student name |\t GPA |\t Major \t ")
+    println("------------------------------------------")
+}
+
+fun endProgram(){
+    println("\nChanges saved.")
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //To print in specific formatted
+    startProgram()
+
+    studentInfo("Rahaf","Nasser",4.71, "Computer Science")
+    studentInfo("Samar","Nasser",4.66, "Computer Science")
+    studentInfo("Norah","Nasser",4.42, "Computer Science")
+    studentInfo("Foton","Nasser",4.38, "Computer Science")
+
+    endProgram()
+
+}
+
